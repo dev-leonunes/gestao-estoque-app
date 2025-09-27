57ca6e3e5da3f1d1a18c3ddbc54b4ec1dd6c41ba
dev-leonunes <leonunes07@outlook.com>
2025-09-26 22:13:09 -0300

    feat: implement dynamic CORS configuration to allow multiple origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,24 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    'https://gestao-estoque-app-frontend.vercel.app',
+    'http://localhost:5173',
+  ];
+
   app.enableCors({
-    origin: 'https://gestao-estoque-app-frontend.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'A política de CORS para este site não permite acesso da Origem especificada.';
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
